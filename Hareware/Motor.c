b8d3580b978f7fa1bbfc002836a4cbbb2e50eb87
JJH-667 <2375795213@qq.com>
2025-11-20 22:47:08 +0800

    直线行驶功能

--- Hareware/Motor.c
+++ Hareware/Motor.c
@@ -1,57 +1,49 @@
-#include "stm32f10x.h"                  // Device header
+#include "stm32f10x.h"                
 #include "PWM.h"
 
-/**
-  * 函    数：直流电机初始化
-  * 参    数：无
-  * 返 回 值：无
-  */
+
 void Motor_Init(void)
 {
-	/*开启时钟*/
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	//开启GPIOB的时钟
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);					//将PB12和PB13引脚初始化为推挽输出
+	GPIO_Init(GPIOB, &GPIO_InitStructure);			
 	
-	PWM_Init();												//初始化直流电机的底层PWM
+	PWM_Init();												
 }
 
-/**
-  * 函    数：直流电机设置PWM
-  * 参    数：PWM 要设置的PWM值，范围：-100~100（负数为反转）
-  * 返 回 值：无
-  */
+
 void Motor1_SetPWM(int8_t PWM)
 {
-	if (PWM >= 0)							//如果设置正转的PWM
+	if (PWM >= 0)							
 	{
-		GPIO_ResetBits(GPIOB, GPIO_Pin_12);	//PB12置低电平
-		GPIO_SetBits(GPIOB, GPIO_Pin_13);	//PB13置高电平
-		PWM_SetCompare3(PWM);				//设置PWM占空比
+		GPIO_ResetBits(GPIOB, GPIO_Pin_12);
+		GPIO_SetBits(GPIOB, GPIO_Pin_13);
+		PWM_SetCompare3(PWM);		
 	}
-	else									//否则，即设置反转的速度值
+	else								
 	{
-		GPIO_SetBits(GPIOB, GPIO_Pin_12);	//PB12置高电平
-		GPIO_ResetBits(GPIOB, GPIO_Pin_13);	//PB13置低电平
-		PWM_SetCompare3(-PWM);				//设置PWM占空比
+		GPIO_SetBits(GPIOB, GPIO_Pin_12);	
+		GPIO_ResetBits(GPIOB, GPIO_Pin_13);	
+		PWM_SetCompare3(-PWM);
 	}
 }
 void Motor2_SetPWM(int8_t PWM)
 {
-	if (PWM >= 0)							//如果设置正转的PWM
+	if (PWM >= 0)						
 	{
-		GPIO_ResetBits(GPIOB, GPIO_Pin_14);	//PB12置低电平
-		GPIO_SetBits(GPIOB, GPIO_Pin_15);	//PB13置高电平
-		PWM_SetCompare3(PWM);				//设置PWM占空比
+		GPIO_ResetBits(GPIOB, GPIO_Pin_14);	
+		GPIO_SetBits(GPIOB, GPIO_Pin_15);	
+		PWM_SetCompare3(PWM);			
 	}
-	else									//否则，即设置反转的速度值
+	else							
 	{
-		GPIO_SetBits(GPIOB, GPIO_Pin_14);	//PB12置高电平
-		GPIO_ResetBits(GPIOB, GPIO_Pin_15);	//PB13置低电平
-		PWM_SetCompare3(-PWM);				//设置PWM占空比
+		GPIO_SetBits(GPIOB, GPIO_Pin_14);	
+		GPIO_ResetBits(GPIOB, GPIO_Pin_15);	
+		PWM_SetCompare3(-PWM);		
 	}
 }
