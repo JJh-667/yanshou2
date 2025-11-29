dc30a544a56ab8ce5d50a0537a99fb5b0730acb2
JJH-667 <2375795213@qq.com>
2025-11-29 16:49:18 +0800

    调参

--- Hareware/Encoder.c
+++ Hareware/Encoder.c
@@ -1,95 +1,92 @@
-#include "stm32f10x.h"                  // Device header
+#include "stm32f10x.h"  
+
+static int16_t EncoderA_LastCnt = 0;
+static int16_t EncoderB_LastCnt = 0;
 
 
 void EncoderA_Init(void)
 {
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);			
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);			
-	
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);			
-		
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;		
-	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;    
-	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; 
-	TIM_TimeBaseInitStructure.TIM_Period = 65536 - 1;           
-	TIM_TimeBaseInitStructure.TIM_Prescaler = 1 - 1;             
-	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;           
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);         
-	
-	TIM_ICInitTypeDef TIM_ICInitStructure;					
-	TIM_ICStructInit(&TIM_ICInitStructure);                 
-	                                                  
-	                                                         
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;         
-	TIM_ICInitStructure.TIM_ICFilter = 0xF;                       
-	TIM_ICInit(TIM3, &TIM_ICInitStructure);                       
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;             
-	TIM_ICInitStructure.TIM_ICFilter = 0xF;                       
-	TIM_ICInit(TIM3, &TIM_ICInitStructure);                    
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStructure.TIM_Period = 65536 - 1;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 1 - 1;
+    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);
 
-	TIM_EncoderInterfaceConfig(TIM3, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Falling);
-																                                                            	                                                            
-	TIM_Cmd(TIM3, ENABLE);			
+    TIM_ICInitTypeDef TIM_ICInitStructure;
+    TIM_ICStructInit(&TIM_ICInitStructure);
+
+    TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+    TIM_ICInitStructure.TIM_ICFilter = 0x0F;
+    TIM_ICInit(TIM3, &TIM_ICInitStructure);
+
+    TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+    TIM_ICInitStructure.TIM_ICFilter = 0x0F;
+    TIM_ICInit(TIM3, &TIM_ICInitStructure);
+
+    TIM_EncoderInterfaceConfig(TIM3, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+    TIM_Cmd(TIM3, ENABLE);
 }
+
 
 void EncoderB_Init(void)
 {
-	
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);		
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-	
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);							
-	
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;				
-	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;     
-	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; 
-	TIM_TimeBaseInitStructure.TIM_Period = 65536 - 1;             
-	TIM_TimeBaseInitStructure.TIM_Prescaler = 1 - 1;                
-	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;           
-	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseInitStructure);            
-	
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStructure.TIM_Period = 65536 - 1;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 1 - 1;
+    TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM4, &TIM_TimeBaseInitStructure);
 
-	TIM_ICInitTypeDef TIM_ICInitStructure;					
-	TIM_ICStructInit(&TIM_ICInitStructure);						
-																
-															
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;			
-	TIM_ICInitStructure.TIM_ICFilter = 0xF;					
-	TIM_ICInit(TIM4, &TIM_ICInitStructure);							
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;		
-	TIM_ICInitStructure.TIM_ICFilter = 0xF;						
-	TIM_ICInit(TIM4, &TIM_ICInitStructure);							
-	
-	TIM_EncoderInterfaceConfig(TIM4, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
-			
-	
-	TIM_Cmd(TIM4, ENABLE);			
+    TIM_ICInitTypeDef TIM_ICInitStructure;
+    TIM_ICStructInit(&TIM_ICInitStructure);
+
+    TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+    TIM_ICInitStructure.TIM_ICFilter = 0x0F;
+    TIM_ICInit(TIM4, &TIM_ICInitStructure);
+
+    TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+    TIM_ICInitStructure.TIM_ICFilter = 0x0F;
+    TIM_ICInit(TIM4, &TIM_ICInitStructure);
+
+    TIM_EncoderInterfaceConfig(TIM4, TIM_EncoderMode_TI12, TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+    TIM_Cmd(TIM4, ENABLE);
 }
+
 
 int16_t EncoderA_Get(void)
 {
-	int16_t Temp;
-	Temp = TIM_GetCounter(TIM3);
-	TIM_SetCounter(TIM3, 0);
-	return Temp;
+    int16_t CurrentCnt = TIM_GetCounter(TIM3);
+    int16_t Diff = CurrentCnt - EncoderA_LastCnt; 
+    EncoderA_LastCnt = CurrentCnt;  
+    return Diff;
 }
+
 
 int16_t EncoderB_Get(void)
 {
-	int16_t Temp;
-	Temp = TIM_GetCounter(TIM4);
-	TIM_SetCounter(TIM4, 0);
-	return Temp;
+    int16_t CurrentCnt = TIM_GetCounter(TIM4);
+    int16_t Diff = CurrentCnt - EncoderB_LastCnt;
+    EncoderB_LastCnt = CurrentCnt;
+    return Diff;
 }
--- Hareware/track.c
+++ Hareware/track.c
@@ -1,6 +1,18 @@
 #include "stm32f10x.h"                  // Device header
 #include "Track.h"
 #include "Motoe.h"
+#include "Encoder.h"
+#include "Delay.h"
+#include "math.h"
+#include "stdlib.h"
+
+float Target1=0, Actual1, Out1, Target2=0, Actual2, Out2;			
+float Kp1=1.25, Ki1=0.19, Kd1=0.01, Kp2=1.25, Ki2=0.2, Kd2=0.01;					
+float Error01, Error11, Error21, Error02, Error12, Error22;
+float Integral1=0,LastOut1=0,Integral2=0,LastOut2=0;
+static float LastActual1 = 0, LastActual2 = 0;
+
+
 
 void Track_Init(void)
 {
@@ -15,46 +27,124 @@ void Track_Init(void)
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
+      
+int8_t PID1(int8_t target)
+{
+     Target1 = target;
+    Actual1 = EncoderA_Get();
+    
+    Error21 = Error11;
+    Error11 = Error01;
+    Error01 = Target1 - Actual1;
+
+    if(fabs(Actual1 - LastActual1) > Target1*0.2) {
+        Error01 = 0;
+    }
+    LastActual1 = Actual1;
+
+    Integral1 += Ki1 * Error01;
+    if(Integral1 > 50) Integral1 = 50;
+    else if(Integral1 < -50) Integral1 = -50;
+    if((LastOut1 >= 90 && Error01 > 0) || (LastOut1 <= -90 && Error01 < 0)) {
+        Integral1 -= Ki1 * Error01;
+    }
+
+    float delta_u = Kp1*(Error01 - Error11) + Integral1 + Kd1*(Error01 - 2*Error11 + Error21);
+
+    #define MAX_STEP 4
+    if(delta_u > MAX_STEP) delta_u = MAX_STEP;
+    else if(delta_u < -MAX_STEP) delta_u = -MAX_STEP;
+
+    Out1 = LastOut1 + delta_u;
+    if(Out1 > 90) Out1 = 90;
+    else if(Out1 < -90) Out1 = -90;
+
+    if(Actual1 < Target1*0.8) Out1 += 10;
+
+    LastOut1 = Out1;
+    return (int8_t)Out1;
+
+}
+
+int8_t PID2(int8_t target)
+{
+    Target2 = target;
+    Actual2 = EncoderB_Get();
+    
+    Error22 = Error12;
+    Error12 = Error02;
+    Error02 = Target2 - Actual2;
+
+    if(fabs(Actual2 - LastActual2) > Target2*0.2) {
+        Error02 = 0;
+    }
+    LastActual2 = Actual2;
+
+    Integral2 += Ki2 * Error02;
+    if(Integral2 > 50) Integral2 = 50;
+    else if(Integral2 < -50) Integral2 = -50;
+    if((LastOut2 >= 90 && Error02 > 0) || (LastOut2 <= -90 && Error02 < 0)) {
+        Integral2 -= Ki2 * Error02;
+    }
+
+    float delta_u = Kp2*(Error02 - Error12) + Integral2 + Kd2*(Error02 - 2*Error12 + Error22);
+
+    #define MAX_STEP 4
+    if(delta_u > MAX_STEP) delta_u = MAX_STEP;
+    else if(delta_u < -MAX_STEP) delta_u = -MAX_STEP;
+
+    Out2 = LastOut2 + delta_u;
+    if(Out2 > 90) Out2 = 90;
+    else if(Out2 < -90) Out2 = -90;
+
+    if(Actual2 < Target2*0.8) Out2 += 10;
+
+    LastOut2 = Out2;
+    return (int8_t)Out2;
+}
+
 void Track_task(void)
 {
-	if(X1==0 && X3==0)
+	if(X1==1 && X3==1 && X2==0 && X4==0)
 	{
 		Motor1_SetPWM(50);
-		Motor2_SetPWM(50);
+		Motor2_SetPWM(55);
 	}
-	if(X1==1 && X3==0)
+	if(X1==1 && X3==0 && X2==0 && X4==0)
 	{
-		Motor1_SetPWM(50);
-		Motor2_SetPWM(0);
+		Motor1_SetPWM(70);
+		Motor2_SetPWM(-70);
 	}
-	if(X1==0 && X3==1)
+	if(X1==0 && X3==1 && X2==0 && X4==0)
 	{
-		Motor1_SetPWM(0);
-		Motor2_SetPWM(50);
-	}
-	if(X1==1 && X2==0 && X3==1 && X4==1)
-	{
-		Motor1_SetPWM(-50);
-		Motor2_SetPWM(50);
-	}
-	if(X1==1 && X2==1 && X3==1 && X4==0)
-	{
-		Motor1_SetPWM(50);
-		Motor2_SetPWM(-50);
-	}
-	if(X1==0 && X2==0 && X3==0 && X4==0)
-	{
-		Motor1_SetPWM(50);
-		Motor2_SetPWM(50);
+		Motor1_SetPWM(-70);
+		Motor2_SetPWM(70);
 	}
 	if(X1==0 && X2==0 && X3==0 && X4==1)
 	{
-		Motor1_SetPWM(50);
-		Motor2_SetPWM(-50);
+		Motor1_SetPWM(-100);
+		Motor2_SetPWM(100);
 	}
-	if(X1==0 && X2==0 && X3==1 && X4==0)
+	if(X1==0 && X2==1 && X3==0 && X4==0)
 	{
-		Motor1_SetPWM(-50);
-		Motor2_SetPWM(50);
+		Motor1_SetPWM(100);
+		Motor2_SetPWM(-100);
 	}
+	if(X1==0 && X2==0 && X3==1 && X4==1)
+	{
+		Motor1_SetPWM(-100);
+		Motor2_SetPWM(100);
+	}
+	if(X1==1 && X2==1 && X3==0 && X4==0)
+	{
+		Motor1_SetPWM(100);
+		Motor2_SetPWM(-100);
+	}
+	if(X1==1 && X2==1 && X3==1 && X4==1)
+	{
+		Motor1_SetPWM(50);
+		Motor2_SetPWM(55);
+	}
+	
 }
+
